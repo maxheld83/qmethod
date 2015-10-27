b632c42d54988af5885c57ceea6ad5704bc39af5
Verena Kasztantowicz <VerenaKasztantowicz@gmail.com>
2015-10-27 18:43:55 +0100

    add support for images in make.cards as per https://github.com/aiorazabala/qmethod/issues/296

--- R/make.cards.R
+++ R/make.cards.R
@@ -31,7 +31,10 @@ make.cards <- function(
   if (!paper.format %in% available.formats) {
     stop("The paper.format specified is not available.")
   }
-  #TODO(maxheld83) add img.dir validation here
+  if (!is.null(img.dir)) {
+    img.dir <- normalizePath(path = img.dir)
+    is.dir(path = img.dir)
+  }
   if (!is.logical(output.pdf)) {
     stop("The argument output.pdf has not been specified logically.")
   }
@@ -56,7 +59,12 @@ make.cards <- function(
     x = q.set[,study.language]
   )
   colnames(q.set.print) <- "full wording"
-
+  if (!is.null(img.dir)) {  # the following happens only it there is an img.dir
+    q.set.print$`full wording` <- paste0("\\includegraphics[width=\\linewidth, height=5.5cm, keepaspectratio=true]{", file_path_sans_ext(q.set), "}")
+    # notice that latex includegraphics needs:
+    #  - paths in quotation marks in case there are spaces in path
+    #  - file path WITHOUT image extension
+  }
   # Create lookup table (same as in import.q.feedback and import.q.sorts!)=====
   if (is.null(manual.lookup)) {  # in case there is no manual lookup
     lookup.table <- apply(  # replace every language field with its hash
@@ -81,38 +89,37 @@ make.cards <- function(
   }
 
   # Add ids to q.set.print ====================================================
-  if (img.dir == NULL) {  # this is for text cards
-    q.set.print$id <- NA  # set up empty id
-    for (handle in rownames(q.set.print)) {  # loop over all ids in q.set
-      if (show.handles) {  # this is for the researcher-facing variant
-        q.set.print[handle, "id"] <- handle
-      } else {
-        q.set.print[handle,"id"] <- lookup.table[handle, study.language]
-      }
+  q.set.print$id <- NA  # set up empty id
+  for (handle in rownames(q.set.print)) {  # loop over all ids in q.set
+    if (show.handles) {  # this is for the researcher-facing variant
+      q.set.print[handle, "id"] <- handle
+    } else {
+      q.set.print[handle,"id"] <- lookup.table[handle, study.language]
     }
-    path <- paste(  # assign path to template
-      path.package("qmethod"),  # where is the package?
-      # remember, "inst" is not in path, because stuff from inst get put in root of package!
-      "/cardtemplates/",
-      paper.format,  # hopefully will have more templates in the future
-      sep = ""
-    )
-    wording.font.size <- wording.font.size  # dumb, but otherwise R complains about unused argument
-  } else {
-
   }
+  q.set.print$id <- unlist(lapply(X = q.set.print$id, FUN = "sanitizeTexString"))
+  # the above makes sure that handle does not include active latex commands
+  #    notice that we DO want latex commands in other columns, so this must be done here "by-hand" and can't be done for all columns via xtable etc.
+  path <- paste(  # assign path to template
+    path.package("qmethod"),  # where is the package?
+    # remember, "inst" is not in path, because stuff from inst get put in root of package!
+    "/cardtemplates/",
+    paper.format,  # hopefully will have more templates in the future
+    sep = ""
+  )
+  wording.font.size <- wording.font.size  # dumb, but otherwise R complains about unused argument
   if (output.pdf == TRUE) {
     return(
       knit2pdf(
         input = path
-        ,output = paste(getwd(),"/",file.name,".tex",sep="")
+        ,output = paste(getwd(), "/", file.name, ".tex", sep="")
       )
     )
   } else {
     return(
       knit(
         input = path
-        ,output = paste(getwd(),"/",file.name,".tex",sep="")
+        ,output = paste(getwd(), "/", file.name, ".tex", sep="")
       )
     )
   }
