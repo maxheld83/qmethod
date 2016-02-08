471bc1ae3e43b217d4e1ac68e0406b19f0904ffa
Maximilian Held <info@maxheld.de>
2016-02-08 20:20:38 +0100

    improve plot for large p-set studies closes https://github.com/aiorazabala/qmethod/issues/344 https://github.com/aiorazabala/qmethod/issues/295

--- R/q.loaplot.R
+++ R/q.loaplot.R
@@ -1,4 +1,4 @@
-q.loaplot <- function(results, quietly = FALSE) {
+q.loaplot <- function(results, quietly = FALSE, names = TRUE, points = FALSE, alpha = 1, density = FALSE, grid = TRUE) {
 
   # Input verification =========================================================
   if (!is.logical(quietly) || !is.vector(quietly) || length(quietly) != 1) {
@@ -11,11 +11,22 @@ q.loaplot <- function(results, quietly = FALSE) {
   # Preliminaries
   loaplots <- combs.plots <- NULL
 
+  # protect against matrix input
+  results$loa <- as.data.frame(results$loa)
+
   # Create all possible plots, including redundant ones ========================
   for (v in colnames(results$loa)) {
     for (h in colnames(results$loa)) {
       g <- ggplot(data = results$loa, mapping = aes_q(x = as.name(v), y = as.name(h), label = rownames(results$loa)))  # the assignment of v and h is weird, but otherwise the axes are the wrong way around. Horizontal should be specified first.
-      g <- g + geom_text()
+      if (names) {
+        g <- g + geom_text()
+      }
+      if (points) {
+        g <- g + geom_point(alpha = alpha)
+      }
+      if (density) {
+        g <- g + geom_density_2d()
+      }
       g <- g + xlim(-1,1) + ylim(-1,1)  # factor loadings always range from -1 to 1, make sure that plots are comparable
       g <- g + coord_fixed()  # distortions of axes are bad and nonsensical
       if (!is.null(results$brief$fcolors)) {  # if factors have colors
@@ -31,13 +42,15 @@ q.loaplot <- function(results, quietly = FALSE) {
   if(!quietly) {  # create convenient all-in-one-plot
     # if always in existence, this stuff *COULD* be read in from the rotations.angle in brief, would avoid duplication and decrease risk of inconsistency
     combs <- combn(x = colnames(results$loa), m = 2, simplify = FALSE)
-    combs
     for (c in combs) {
       name <- paste(c[1], c[2], sep = "-")
       combs.plots[[name]] <- loaplots[[c[1]]][[c[2]]]
     }
-    do.call(what = "grid.arrange", args = c(combs.plots, ncol = ceiling(sqrt(length(combs)))))
+    if (grid) {
+      do.call(what = "grid.arrange", args = c(combs.plots, ncol = ceiling(sqrt(length(combs)))))
+    } else {
+      print(combs.plots)
+    }
   }
-
   return(invisible(loaplots))  # return them all
 }
