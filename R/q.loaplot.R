f9fd2c2aff1d34f47f9ed3b2bf54c287e67454cf
Maximilian Held <info@maxheld.de>
2016-02-11 21:34:08 +0100

    enable roxygen, start work on https://github.com/aiorazabala/qmethod/issues/31

--- R/q.loaplot.R
+++ R/q.loaplot.R
@@ -1,4 +1,50 @@
-q.loaplot <- function(results, quietly = FALSE, names = TRUE, points = FALSE, alpha = 1, density = FALSE, grid = TRUE) {
+#' @title Q methodology: create Q factor loadings plot
+#'
+#' @description Plots the loadings of Q-sorts on all factors against all factors.
+#'
+#' @param results A results object from \code{\link{qmethod}} or a loadings table.
+#' @param names Logical, indicating whether rownames (of Q-sorts) from the loadings table should be printed.
+#' Should be set to \code{FALSE} when there are very many Q-sorts, or when there are no meaningful names.
+#' @param points Logical, indicating whether a point should be plotted at the precise location of a Q-sort.
+#' Defaults to \code{FALSE}.
+#' Defaults to \code{TRUE}.
+#' @param alpha Numerical vector of length one between 0 and 1, indicating transparency of points.
+#' Set to below 1 to avoid overplotting.
+#' Defaults to \code{1}, in which case points are not transparent.
+#' @param density Logical, indicating whether a 2-d density estimate should be added.
+#' Helpful for very many Q-sorts with much overplotting.
+#' Defaults to \code{FALSE}.
+#' @param grid Logical, indicating whether the \emph{combinations} of plots should be returned as a grid or individually, when \code{quietly = FALSE}.
+#' Defaults to \code{TRUE}.
+#' @param quietly Logical, indicating whether a summary plot should be printed.
+#' Defaults to \code{FALSE}.
+#' @details During rotation procedures and final interpretation in Q methodology, researchers need to inspect the loadings of participant Q-sorts (as people-variables) on the extracted factors.
+#' Instead of a tabular view, this function creates a simple plot.
+#' @return Returns a list of lists of \pkg{ggplot2} objects, one for each pair of extracted factors.
+#' @author Maximilian Held
+#' @note The returned object includes all possible \emph{permutations} of factor pairs.
+#' Because the plot of factor A against factor B is the same as factor B against factor A, only with transposed axes, half of these plots are redundant.
+#' They are included to allow researchers to conveniently access any given pair of factors.
+#'
+#' The function also returns a grid of all \emph{non-redundant} factor pairs, that is, the \emph{combinations} of factor pairs.
+#'
+#' If there are factor names from \code{\link{q.fnames}} or colors from \code{\link{q.fcolors}} in \code{results}, axes will be named and colored accordingly.
+#' Remember that factor names and colors may be specific to a particular rotation procedure, and may not be appropriate before rotations are completed.
+#' @examples
+#' data(lipset)  # use data included with this package
+#' results <- qmethod(lipset[[1]], nfactors=3, rotation="varimax")  # run analysis
+#' loaplots <- q.loaplot(results = results)  # produces plots list
+#' loaplots$f1$f2  # retrieves loaplot of f1 vs f2
+
+q.loaplot <- function(results,
+                      names = TRUE,
+                      points = FALSE,
+                      alpha = 1,
+                      density = FALSE,
+                      grid = TRUE,
+                      #flaglines = FALSE,
+                      #nstat,
+                      quietly = FALSE) {
 
   # Input verification =========================================================
   if (!is.logical(quietly) || !is.vector(quietly) || length(quietly) != 1) {
